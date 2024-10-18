448937170ee70d90047a7aa534b9235f02cfec2c
Osvaldo Paz <osvaldotws@gmail.com>
2024-10-18 13:48:40 -0300

    os-ejemplo

---
+++ os-ejemplo/index.mjs
@@ -0,0 +1,17 @@
+import os from 'os';
+
+// Obtener el tipo de sistema operativo
+console.log('Arquitectura:', os.arch());
+
+// Obtener el tipo de sistema operativo
+console.log('Plataforma:', os.platform());
+
+// Obtener la cantidad total de memoria
+console.log('Memoria total:', os.totalmem());
+
+// Obtener la memoria libre
+console.log('Memoria libre:', os.freemem());
+
+// Obtener la información de la CPU
+console.log('Informacion de la CPU', os.cpus())
+
